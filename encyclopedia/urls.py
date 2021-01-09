2c3322a07a4fa22cc810cdab98085550a0d5fdaf
leny133 <63651131+leny133@users.noreply.github.com>
2021-01-09 15:56:13 -0400

    organize form unfinished

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "tasks"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.search, name="search")
+    path("wiki/<str:title>", views.title, name="title"),
+    path("wiki", views.search, name="search")
 ]
